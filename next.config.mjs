ae1eae1aa9c583fa6bcd53eef8b5b9c7e82a0736
Yadhukrishnan <93711864+yadhukrixh@users.noreply.github.com>
2025-02-20 13:22:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import type { NextConfig } from "next";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
 
 export default nextConfig;
